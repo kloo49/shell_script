7b71c4086640c9f4ede25441b672a625df93ba51
Kelly Loo <kyloo@nbarton-eth1.ucsd.edu>
2025-09-04 16:05:35 -0700

    Added Tropical Atlantic

--- opendaily.sh
+++ opendaily.sh
@@ -1,5 +1,17 @@
 # !/bin/bash/
 
+'''
+This code opens all the links that needs to be checked daily.
+- Tropical Tidbits (Recon and Storm Info)
+- Tropical Atlantic (Current recon)
+- NHC POD and tropical cyclone watch
+- C-130s, P3, and G-IV on FlightAware
+
+INPUT: None
+
+OUTPUT: Websites mentioned above opens in a new window.
+'''
+
 cd /Applications/ 
 echo "Starting process..."
 echo ""
@@ -10,9 +22,9 @@ browser="Google Chrome"
 
 echo "Loading websites"
 websites=("https://www.tropicaltidbits.com/recon/
+https://tropicalatlantic.com/recon/recon.cgi?all=1
 https://www.tropicaltidbits.com/storminfo/
 https://www.nhc.noaa.gov/
-https://www.nhc.noaa.gov/text/MIAREPRPD.shtml
 https://tropic.ssec.wisc.edu/
 https://www.flightaware.com/live/flight/965300 
 https://www.flightaware.com/live/flight/965301 
@@ -27,7 +39,8 @@ https://www.flightaware.com/live/flight/995309
 https://www.flightaware.com/live/flight/N42RF
 https://www.flightaware.com/live/flight/N43RF
 https://www.flightaware.com/live/flight/N49RF
-https://www.dropbox.com/scl/fi/7vm0j174846pcawofppav/tc25_recovery_datalog.xlsx?rlkey=bs9aw3gzit8q3658b0w927916&st=1prki027&dl=0")
+https://www.dropbox.com/scl/fi/7vm0j174846pcawofppav/tc25_recovery_datalog.xlsx?rlkey=bs9aw3gzit8q3658b0w927916&st=1prki027&dl=0
+https://www.nhc.noaa.gov/text/MIAREPRPD.shtml")
 echo " "
 
 open -na "$browser" --args --new-window $websites

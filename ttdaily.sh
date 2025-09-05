f44791d35627b154df749ceb6eca73c281432959
Kelly Loo <kyloo@nbarton-eth1.ucsd.edu>
2025-09-05 11:31:47 -0700

    Comments

--- ttdaily.sh
+++ ttdaily.sh
@@ -1,41 +1,71 @@
 # !/bin/bash
 
-# create folder in screenshots_tropical_tidbits
-cd "$HOME/agsd Dropbox/agsd's shared workspace/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits"
+: << 'CONVENTION'
+This code saves Tropical Tidbit screenshots. This includes recon and storm information.
+For storms, files must be saved as DATE_STORM_track.png or DATE_STORM_intensity.png
+For flights, do not change the file name when you save it from the website.
 
-echo -n "Enter flight date (YYYY-MM-DD): "
-read -r date
+INPUT:
+- User input: y/n
+If y,
+    - User input: date the flight
+    - User input: tail number
 
-echo -n "Enter tail number: "
-read -r tail
+OUTPUT: 
+- Recon screenshots moved to /screenshot_tropical_tidbits/DATE_TAIL
+- Storm screenshots moved to /screenshot_tropical_tidbits
 
-# TODO: add conditional storm name input
+NOTES: NOAA flight recon has not been tested yet.
+CONVENTION
 
-tailname=$(echo "$tail" | tr '[:upper:]' '[:lower:]' )
-mkdir "$date"_"$tailname"
+# # path
+dropbox_path="$HOME/agsd Dropbox/agsd's shared workspace"
+tt_path="$dropbox_path""/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits"
+download_path="$HOME/Downloads"
 
-# fix case sensitive
+# # functions
+stormforecasts () {
+    for f in "$1"; do
+        mv "$f" "$tt_path"
+        echo "$2 screenshot saved."
+    done
+}
 
-tailfind=$(echo "$tail" | tr '[:lower:]' '[:upper:]' )
-mv /Users/kyloo/Downloads/recon_$tailfind* /Users/kyloo/agsd\ Dropbox/agsd\'s\ shared\ workspace/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits/"$date"_"$tailname"/
+# # initializing code
+echo -n "Archiving flight? (y/n): "
+read -r yn
 
-# Report result
-if [[ $? -ne 0 ]]; then
-    echo "No matching recon files found for tail $tailfind in Downloads."
+if [ $yn == "y" ]; then
+    # # create folder in screenshots_tropical_tidbits
+    echo -n "Enter flight date (YYYY-MM-DD): "
+    read -r date
+
+    echo -n "Enter tail number: "
+    read -r tail
+
+    # Change to lowercase to create directory
+    cd "$tt_path"
+    tailname=$(echo "$tail" | tr '[:upper:]' '[:lower:]' )
+    mkdir "$date"_"$tailname"
+
+    # # Find the file
+    # Change to uppercase to find file in Downloads folder
+    tailfind=$(echo "$tail" | tr '[:lower:]' '[:upper:]' )
+    noaa=(["n42rf"]="NOAA2" ["n43rf"]="NOAA3" ["n49rf"]="NOAA9")
+    if [ $tailfind == $noaa[@] ];then
+        for key in "${!noaa[@]}";do
+            echo "NOAA flight tail $key aircraft ${noaa[$key]}"
+        done
+    else
+        mv "$download_path"/recon_$tailfind* "$tt_path"/"$date"_"$tailname"/
+        echo "C-130 Tropical Tidbits screenshots moved."
+    fi
+
+    # # TODO: add conditional storm name input
+
 else
-    echo "Moved recon files to ${date}_${tailname}/"
+    track=($(find "$download_path" -name "*_track.png*"))
+    intensity=($(find "$download_path" -name "*_intensity.png*"))   
+    stormforecasts "${track[@]}" "Track"
+    stormforecasts "${intensity[@]}" "Intensity"
 fi
-
-# cd /Users/kyloo/Downloads/
-# if [ -e "*_tracks.png" ]; then
-#     mv /Users/kyloo/Downloads/*tracks.png /Users/kyloo/agsd\ Dropbox/agsd\'s\ shared\ workspace/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits/
-#     echo "Tracks screenshots are in screenshots_tropical_tidbits."
-#     if [ -e "*_intensity.png" ]; then
-#         mv /Users/kyloo/Downloads/*intensity.png /Users/kyloo/agsd\ Dropbox/agsd\'s\ shared\ workspace/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits/
-#         echo "Intensity screenshots are in screenshots_tropical_tidbits."
-#     else
-#         echo "Intensity screenshots do not exist."
-#     fi
-# else
-#     echo "Tracks screenshots do not exist."
-# fi

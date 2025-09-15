23876940d678898885cdca05d232cc41c050e988
Kelly Loo <kyloo@nbarton-eth1.ucsd.edu>
2025-09-15 15:07:36 -0700

    code works now

--- c130insitu.sh
+++ c130insitu.sh
@@ -13,17 +13,9 @@ ags2025="$ags_path/2025.181_tc2025/c130-insitu_usaf"
 # code_kloo
 cd "$HOME/ags/projects/hiaper/code_kloo"
 
-filename=()
+pythonfilename=()
 for line in $(cat newfilename.txt);do 
-    filename+=("$line") # .txt file to list
-done
-
-# isolate mission name from the new naming convention
-
-agsmissionname=()
-for line in "${filename[@]}";do
-    mission="${line:11}"
-    agsmissionname+=("$mission") # a list of mission names to match with the Google Drive
+    pythonfilename+=("$line") # .txt file to list
 done
 
 #---------------------------------------------------------------------------------------------------------
@@ -32,22 +24,22 @@ echo -n "Enter TC campaign year (YYYY): "
 read -r year
 
 rename () {
-    gfile=($(find . -maxdepth 1 -name "*.txt"))
-    gmissionname=()
-
-    for file in "${gfile[@]}"; do
-        gmission="${file:2}"
-        gmissionname+=($gmission)
-    done
+    agsfile=($(find . -maxdepth 1 -name "*.txt")) # puts file name from c130-insitu-usaf in a list
     
-    i=0
-    for file in "${gmissionname[@]}";do
-        if [[ "${agsmissionname[*]}" == *"$file"* ]];then
-            echo "$file is present in both arrays. New file name is: "${filename[i]}"."
-            i=$(($i+1))
-            # mv "$gmissionname" "$file"
+    agsmissionname=() # cleans up $agsfile
+    for file in "${agsfile[@]}"; do
+        agsmission="${file:2}"
+        # echo "agsfile is $file and remove first two is $agsmission"
+        agsmissionname+=("$agsmission")
+    done
+
+    for line in "${pythonfilename[@]}"; do
+        match="${line:11}"
+        if [[ "${agsmissionname[*]}" == *"$match"* ]]; then
+            echo "$match is present in both arrays. New file name is: $line."
+            mv "$match" "$line"
         else
-            echo "$file is not present in both arrays. Check again."
+            echo "$match is not present in both arrays."
         fi
     done
 }
@@ -55,16 +47,16 @@ rename () {
 if [[ $year == "2024" ]]; then
     echo "Campaign ID: 2024.162_tc2024"
     cd "$dropbox_path/tc24_usaf_aro/1_complete"
-    one_second_data=($(find . -name "*01.txt"))
-    # echo $one_second_data
-    # cp $one_second_data $ags2024
+    one_second_data=$(find . -name "*01.txt")
+    cp $one_second_data $ags2024
     cd "$ags2024"
-    # find . -maxdepth 1 -name "*.txt"
     rename
 elif [[ $year == "2025" ]]; then
     echo "Campaign ID: 2025.181_tc2025"
     cd "$dropbox_path/tc25_usaf_aro/1_complete"
     one_second_data=($(find . -name "*01.txt"))
+    cp $one_second_data $ags2025
+    cd "$ags2025"
     rename
 fi
 

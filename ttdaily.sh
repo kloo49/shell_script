f755e28d94f1798c663e90b0a6614200860e6356
Kelly Loo <kyloo@nbarton-eth1.ucsd.edu>
2025-09-22 12:55:12 -0700

    updated download location

--- ttdaily.sh
+++ ttdaily.sh
@@ -9,7 +9,7 @@ INPUT:
 - User input: y/n
 If y,
     - User input: date the flight
-    - User input: tail number
+    - User input: aircraft type
 
 OUTPUT: 
 - Recon screenshots moved to /screenshot_tropical_tidbits/DATE_TAIL
@@ -21,14 +21,18 @@ CONVENTION
 # # path
 dropbox_path="$HOME/agsd Dropbox/agsd's shared workspace"
 tt_path="$dropbox_path""/data_temp/tc25_usaf_aro/screenshot_tropical_tidbits"
-download_path="$HOME/Downloads"
+download_path="$HOME/Desktop/aircraft-imgs"
 
 # # functions
 stormforecasts () {
-    for f in "$1"; do
-        mv "$f" "$tt_path"
-        echo "$2 screenshot saved."
-    done
+    if [ -z "$1" ];then
+        echo "No $2 screenshots found."
+    else
+        for f in "$1"; do
+            mv "$f" "$tt_path"
+            echo "$2 screenshot saved."
+        done
+    fi
 }
 
 # # initializing code
@@ -40,7 +44,7 @@ if [ $yn == "y" ]; then
     echo -n "Enter flight date (YYYY-MM-DD): "
     read -r date
 
-    echo -n "Enter tail number: "
+    echo -n "Enter aircraft (AF30x/NOAAx): "
     read -r tail
 
     # Change to lowercase to create directory
@@ -51,18 +55,8 @@ if [ $yn == "y" ]; then
     # # Find the file
     # Change to uppercase to find file in Downloads folder
     tailfind=$(echo "$tail" | tr '[:lower:]' '[:upper:]' )
-    noaa=(["n42rf"]="NOAA2" ["n43rf"]="NOAA3" ["n49rf"]="NOAA9")
-    if [ $tailfind == $noaa[@] ];then
-        for key in "${!noaa[@]}";do
-            echo "NOAA flight tail $key aircraft ${noaa[$key]}"
-        done
-    else
-        mv "$download_path"/recon_$tailfind* "$tt_path"/"$date"_"$tailname"/
-        echo "C-130 Tropical Tidbits screenshots moved."
-    fi
-
-    # # TODO: add conditional storm name input
-
+    mv "$download_path"/recon_$tailfind* "$tt_path"/"$date"_"$tailname"/
+    echo "Tropical Tidbits flight screenshots moved."
 else
     track=($(find "$download_path" -name "*_track.png*"))
     intensity=($(find "$download_path" -name "*_intensity.png*"))   
